b48fb6bbeb642c394ecf81c501a1e5c8fc3779f7
Aman Gupta <aman2304@gmail.com>
2016-08-12 12:53:58 -0700

    Fixed bug

--- overlap_nest.m
+++ overlap_nest.m
@@ -1,4 +1,4 @@
-function [x, costs] = overlap_nest(f, gradf, gamma, L, x0, k, iters_acc, eps_acc)
+function [x, costs] = overlap_nest(f, gradf, gamma, L, x0, k, iters_acc, eps_acc, A_potential, A_bad, C, num_cascades, i)
 
 % Minimizes regularization functional 
 % f(w) + gamma/2 ||w||^2
@@ -14,11 +14,11 @@ alpha = x0;
 x = x0;
 d = length(x);
 prev_cost = inf;
-curr_cost = feval(f,x0) + gamma/2 * ( norm_overlap(x0,k) )^2;
+curr_cost = feval(f,x0,A_potential, A_bad, C, num_cascades,i) + gamma/2 * ( norm_overlap(x0,k) )^2;
 theta = 1;
 
 while (t < iters_acc && abs(prev_cost - curr_cost) > eps_acc)
-  gradalpha = feval(gradf,alpha);
+  gradalpha = feval(gradf,alpha,A_potential, A_bad, C, num_cascades, i);
   prevx = x;
   x = prox_overlap( -1/L* gradalpha + alpha, k, L/gamma);
   theta = (sqrt(theta^4+4*theta^2)-theta^2)/2;
@@ -26,7 +26,7 @@ while (t < iters_acc && abs(prev_cost - curr_cost) > eps_acc)
   alpha = rho*x - (rho-1)*prevx;
   t = t+1;
   prev_cost = curr_cost;
-  curr_cost = feval(f,alpha) + gamma/2 * ( norm_overlap(alpha,k) )^2;
+  curr_cost = feval(f,alpha,A_potential, A_bad, C, num_cascades, i) + gamma/2 * ( norm_overlap(alpha,k) )^2;
   costs(t-1) = curr_cost;
 end
 
--- run_ksupport.m
+++ run_ksupport.m
@@ -12,4 +12,4 @@ k = 4;
 iters_acc = 100;
 eps_acc = 1e-3;
 
-[x, costs] = overlap_nest(@calc_obj, @calc_grad, gamma, L, x0, k, iters_acc, eps_acc);
+[x, costs] = overlap_nest(@calc_obj, @calc_grad, gamma, L, x0, k, iters_acc, eps_acc, A_potential, A_bad, C, num_cascades, i);
